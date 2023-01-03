3461941b96e40450ad2b139a6c4d4475ee7dc068
White Knight Labs <97109724+WKL-Sec@users.noreply.github.com>
2023-01-03 13:58:54 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IPADDRESS="$(ip address | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d''/)"
+
+cd /opt/cobaltstrike
+
+# Check if we already downloaded CS teamserver or do we grab a fresh copy
+FILE=/opt/cobaltstrike/cs-download-check
+
+if [ -f "$FILE" ]; then
+    /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/profiles/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
+else
+	touch /opt/cobaltstrike/cs-download-check
+    token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
+	curl -s https://download.cobaltstrike.com/downloads/${token}/latest46/cobaltstrike-dist.tgz -o /tmp/cobaltstrike.tgz
+	tar zxf /tmp/cobaltstrike.tgz -C /opt
+	echo ${COBALTSTRIKE_KEY} | /opt/cobaltstrike/update
+	/opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/profiles/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
+fi
