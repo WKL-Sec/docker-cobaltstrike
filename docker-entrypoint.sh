fbdf5bdd3509cd3d312964f7037edc350acf6982
White Knight Labs <97109724+WKL-Sec@users.noreply.github.com>
2023-01-03 19:41:30 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ cd /opt/cobaltstrike
 FILE=/opt/cobaltstrike/cs-download-check
 
 if [ -f "$FILE" ]; then
-    /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/profiles/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
+    /opt/cobaltstrike/teamserver $IPADDRESS ${COBALTSTRIKE_PASS} /opt/cobaltstrike/${COBALTSTRIKE_PROFILE} ${COBALTSTRIKE_EXP}
 else
 	touch /opt/cobaltstrike/cs-download-check
     token=`curl -s https://download.cobaltstrike.com/download -d "dlkey=${COBALTSTRIKE_KEY}" | grep 'href="/downloads/' | cut -d '/' -f3`
